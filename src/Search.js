5a974516e1e297167d80e5f23e08a576d74a5be6
h4nie <hniazd@gmail.com>
2022-06-07 18:23:37 -0400

    added date range picker

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Search.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
 
 function Search() {
-  return <div className="search">Search</div>;
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+    </div>
+  );
 }
 
 export default Search;

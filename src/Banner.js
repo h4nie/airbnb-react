782d53c5620d6d37ad56932db7308686f7b7e5ab
h4nie <hniazd@gmail.com>
2022-06-18 12:48:47 -0400

    added navigate

--- src/Banner.js
+++ src/Banner.js
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@mui/material";
 import Search from "./Search";
+import { useNavigate } from "react-router-dom";
 
 function Banner() {
+  const navigate = useNavigate();
   const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner">
@@ -22,7 +24,9 @@ function Banner() {
         <h5>
           Plan a different kind of getaway to uncover the hidden gems near you.
         </h5>
-        <Button variant="outlined">Explore Nearby</Button>
+        <Button variant="outlined" onClick={() => navigate("./search")}>
+          Explore Nearby
+        </Button>
       </div>
     </div>
   );

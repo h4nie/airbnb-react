339ab364e7a58cac6e4d4fd8a8af1af83aa9e6a8
h4nie <hniazd@gmail.com>
2022-06-18 11:36:28 -0400

    added search page and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
